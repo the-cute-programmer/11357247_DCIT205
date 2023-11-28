ba4a8a2fd18914baf833ecb3f1b65306c38408bc
the-cute-programmer <roduronyarko@gmail.com>
2023-11-28 23:05:25 +0000

    Added new content

---
+++ index.js
@@ -0,0 +1,5 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+
+// This updates the content of the span with id "currentYear" with the current year
+document.getElementById('currentYear').textContent = currentYear;
